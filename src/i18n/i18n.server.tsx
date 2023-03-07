7a6d5791e614cb1d83f6ea714ea96513acc453b3
gbuomprisco <giancarlopsk@gmail.com>
2023-03-07 18:39:05 +0800

    Updated packages, removed fs-backend for i18n

--- src/i18n/i18n.server.tsx
+++ src/i18n/i18n.server.tsx
@@ -1,25 +1,52 @@
+import { readFileSync } from 'fs';
+
 import { createInstance } from 'i18next';
 import { initReactI18next } from 'react-i18next/initReactI18next';
-import { join } from 'path';
-
-import Backend from 'i18next-fs-backend';
 import getI18nSettings from './i18n.settings';
 
 async function initializeServerI18n(lang?: Maybe<string>) {
   const i18nInstance = createInstance();
   const settings = getI18nSettings(lang);
 
-  await i18nInstance
-    .use(initReactI18next)
-    .use(Backend)
-    .init({
-      ...settings,
-      backend: {
-        loadPath: join(process.cwd(), './public/locales/{{lng}}/{{ns}}.json'),
-      },
-    });
+  await i18nInstance.use(initReactI18next).init({
+    ...settings,
+    resources: {
+      [settings.lng]: createLanguageBundle(settings.lng),
+    },
+  });
 
   return i18nInstance;
 }
 
 export default initializeServerI18n;
+
+function createLanguageBundle(language: string) {
+  const common = readTranslationFile(language, 'common');
+  const auth = readTranslationFile(language, 'auth');
+  const organization = readTranslationFile(language, 'organization');
+  const profile = readTranslationFile(language, 'profile');
+  const subscription = readTranslationFile(language, 'subscription');
+
+  return {
+    common,
+    auth,
+    organization,
+    profile,
+    subscription,
+  };
+}
+
+function readTranslationFile(language: string, fileName: string) {
+  try {
+    const prefix = `${process.cwd()}/public/locales`;
+    const file = readFileSync(`${prefix}/${language}/${fileName}.json`, 'utf8');
+
+    return JSON.parse(file);
+  } catch (e) {
+    console.error(
+      `Error: failed to read translation file: ${fileName}. Does it exist?`
+    );
+
+    return {};
+  }
+}
