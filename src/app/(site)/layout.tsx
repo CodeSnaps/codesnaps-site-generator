33599a4358e8ef08a28899c96c9da803a3322980
gbuomprisco <giancarlopsk@gmail.com>
2023-03-16 20:52:46 +0800

    Init i18n in site layout

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -3,9 +3,13 @@ import { use } from 'react';
 import Footer from '~/app/(site)/components/Footer';
 import SiteHeaderSessionProvider from '~/app/(site)/components/SiteHeaderSessionProvider';
 import loadUserData from '~/lib/server/loaders/load-user-data';
+import initializeServerI18n from '~/i18n/i18n.server';
+import getLanguageCookie from '~/i18n/get-language-cookie';
 
-function SiteLayout(props: React.PropsWithChildren) {
+async function SiteLayout(props: React.PropsWithChildren) {
   const data = use(loadUserData());
+
+  await initializeServerI18n(getLanguageCookie());
 
   return (
     <main>
