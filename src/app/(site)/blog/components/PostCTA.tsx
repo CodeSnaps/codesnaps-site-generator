54b84ff1b0d2469412f3359e2b89fbf930c624f3
kaumonaung <47107420+kaumonaung@users.noreply.github.com>
2023-11-09 19:01:52 +0100

    Fixed ESLint error

--- src/app/(site)/blog/components/PostCTA.tsx
+++ src/app/(site)/blog/components/PostCTA.tsx
@@ -8,8 +8,8 @@ const PostCTA: React.FC<{}> = () => {
         <div className="py-8 px-10 border-2 border-primary-500 dark:border-primary-400 rounded-xl my-14 text-lg leading-relaxed bg-primary-400/10">
           <div>👋 Hi there!</div>
           Wanna accelerate your app development with React and Tailwind CSS?
-          Explore the library I'm crafting to build faster and better with React
-          and Tailwind CSS.
+          Explore the library I&apos;m crafting to build faster and better with
+          React and Tailwind CSS.
           <div className="mt-4">
             <span className="underline">Learn more</span>
           </div>
