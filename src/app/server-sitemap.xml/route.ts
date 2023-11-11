726958e14417e0915cfefc89a96919e5fcf4cdb4
kaumonaung <47107420+kaumonaung@users.noreply.github.com>
2023-11-11 01:27:15 +0100

    Configured server sitemap

--- src/app/server-sitemap.xml/route.ts
+++ src/app/server-sitemap.xml/route.ts
@@ -3,6 +3,8 @@ import { getServerSideSitemap } from 'next-sitemap';
 import { allPosts } from 'contentlayer/generated';
 import configuration from '~/configuration';
 
+import { categories } from '~/lib/components/database/filter-list';
+
 const siteUrl = configuration.site.siteUrl as string;
 
 if (!siteUrl) {
@@ -12,12 +14,13 @@ if (!siteUrl) {
 export async function GET() {
   const urls = getSiteUrls();
   const posts = getPostsSitemap();
+  const components = getAllComponents();
 
-  return getServerSideSitemap([...urls, ...posts]);
+  return getServerSideSitemap([...urls, ...posts, ...components]);
 }
 
 function getSiteUrls() {
-  const urls = ['', 'faq', 'pricing', 'components'];
+  const urls = ['', 'faq', 'pricing', 'browse'];
 
   return urls.map((url) => {
     return {
@@ -31,6 +34,17 @@ function getPostsSitemap() {
   return allPosts.map((post) => {
     return {
       loc: join(siteUrl, post.url),
+      lastmod: new Date().toISOString(),
+    };
+  });
+}
+
+function getAllComponents() {
+  const remainingCategories = categories.slice(1);
+
+  return remainingCategories.map((category) => {
+    return {
+      loc: join(siteUrl, category.href),
       lastmod: new Date().toISOString(),
     };
   });
