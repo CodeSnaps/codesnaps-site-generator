dec46f5c68ee912c8522433e52b7a2501d05d814
kaumonaung <47107420+kaumonaung@users.noreply.github.com>
2023-11-22 19:23:39 +0100

    Removed proxy since it wasn't working

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 const withAnalyzer = require('@next/bundle-analyzer');
 const { withContentlayer } = require('next-contentlayer');
-const { withPlausibleProxy } = require('next-plausible');
 
 const IS_PRODUCTION = process.env.NODE_ENV === 'production';
 const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
@@ -14,7 +13,7 @@ const nextConfig = {
 
 module.exports = withAnalyzer({
   enabled: process.env.ANALYZE === 'true',
-})(withContentlayer(withPlausibleProxy()(nextConfig)));
+})(withContentlayer(nextConfig));
 
 function getRemotePatterns() {
   // add here the remote patterns for your images
